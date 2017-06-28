eaf89821824bff1bf28684f06eeb000a89a95978
Adam Malone <adam@adammalone.net>
2017-06-28 16:49:11 +1000

    Adds more commands for deployment purposes.

--- src/Commands/DeployCommand.php
+++ src/Commands/DeployCommand.php
@@ -29,15 +29,13 @@ class DeployCommand extends Tasks
         $this->cloudapi = $cloudapi;
     }
 
-    /**
-     * This is the acquia:deploy command
-     *
-     * @command acquia:deploy
-     */
-    public function acquiaDeploy($site, $environment) {
+    protected function acquiaDeployEnv($site, $environment) {
+        $site = $this->cloudapi->site($site);
+        $siteName = $site->unixUsername();
+
         $task = $this->taskDrushStack()
             ->stopOnFail()
-            ->siteAlias("@${site}.${environment}")
+            ->siteAlias("@${siteName}.${environment}")
             ->clearCache('drush')
             ->drush("cache-rebuild")
             ->updateDb()
@@ -48,9 +46,52 @@ class DeployCommand extends Tasks
     }
 
     /**
+     * This is the acquia:preproddeployenv command
+     *
+     * @command acquia:deploy:preprod:env
+     */
+    public function acquiaDeployPreProdEnv($site, $environment) {
+        if ($environment == 'prod') {
+            throw new \Exception('Use the acquia:proddeploy command for the production environment.');
+        }
+
+        $this->acquiaDeployEnv($site, $environment);
+    }
+
+    /**
+     * This is the acquia:preproddeploy command
+     *
+     * @command acquia:deploy:preprod
+     */
+    public function acquiaDeployPreProd($site) {
+        $environments = $this->cloudapi->environments($site);
+
+        foreach ($environments as $environment) {
+            $env = $environment->name();
+            if ($env == 'prod') {
+                continue;
+            }
+
+            $this->acquiaDeployEnv($site, $env);
+        }
+    }
+
+    /**
+     * This is the acquia:proddeploy command
+     *
+     * @command acquia:deploy:prod
+     */
+    public function acquiaDeployProd($site) {
+        $this->yell('WARNING: DEPLOYING TO PROD');
+        if ($this->confirm('Are you sure you want to deploy to prod?')) {
+            $this->acquiaDeployEnv($site, 'prod');
+        }
+    }
+
+    /**
      * This is the acquia:sites command
      *
-     * @command acquia:sites
+     * @command acquia:site:list
      */
     public function acquiaSites()
     {
@@ -63,7 +104,7 @@ class DeployCommand extends Tasks
     /**
      * This is the acquia:siteinfo command
      *
-     * @command acquia:siteinfo
+     * @command acquia:site:info
      */
     public function acquiaSiteInfo($site)
     {
@@ -96,9 +137,9 @@ class DeployCommand extends Tasks
     /**
      * This is the acquia:preprodprep command
      *
-     * @command acquia:preprodprep
+     * @command acquia:prepare:preprod
      */
-    public function acquiaPreProdPrep($site)
+    public function acquiaPreparePreProd($site)
     {
         $environments = $this->cloudapi->environments($site);
         foreach ($environments as $environment) {
@@ -128,7 +169,7 @@ class DeployCommand extends Tasks
     /**
      * This is the acquia:tasks command
      *
-     * @command acquia:tasks
+     * @command acquia:task:list
      */
     public function acquiaTasks($site) {
 
@@ -150,7 +191,7 @@ class DeployCommand extends Tasks
     /**
      * This is the acquia:task command
      *
-     * @command acquia:task
+     * @command acquia:task:info
      */
     public function acquiaTask($site, $taskId) {
         $tz = 'Australia/Sydney';
