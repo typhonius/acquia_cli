876af104d7aaa087a15b8ef5df8a7c8c2300ba6d
Adam Malone <adam@adammalone.net>
2017-11-06 13:53:05 +1000

    Adds additional filters/sorts to tasks.

--- src/Commands/InfoCommand.php
+++ src/Commands/InfoCommand.php
@@ -51,26 +51,44 @@ class InfoCommand extends AcquiaCommand
      * Gets all tasks associated with a site.
      *
      * @param string $uuid
+     * @param int    $limit  The maximum number of items to return.
+     * @param string $filter
+     * @param string $sort   Sortable by: 'name', 'title', 'created', 'completed', 'started'.
+     * A leading "~" in the field indicates the field should be sorted in a descending order.
      *
      * @command task:list
      * @alias t:l
      */
-    public function acquiaTasks($uuid)
+    public function acquiaTasks($uuid, $limit = 100, $filter = null, $sort = '~created')
     {
 
-        // @TODO sort by most recent to least
-
+        // Allows for limits and sort criteria.
+        str_replace('~', '-', $sort);
+        $this->cloudapi->addQuery('limit', $limit);
+        $this->cloudapi->addQuery('sort', $sort);
+        if (null !== $filter) {
+            $this->cloudapi->addQuery('filter', "name=${filter}");
+        }
         $tasks = $this->cloudapi->tasks($uuid);
+        $this->cloudapi->clearQuery();
 
         $output = $this->output();
         $table = new Table($output);
-        $table->setHeaders(['ID', 'Name', 'Status', 'User']);
+        $table->setHeaders(['ID', 'Created', 'Name', 'Status', 'User']);
+
+        $tz = $this->extraConfig['timezone'];
+        $format = $this->extraConfig['format'];
+        $timezone = new \DateTimeZone($tz);
 
         foreach ($tasks as $task) {
+            $createdDate = new \DateTime($task->createdAt);
+            $createdDate->setTimezone($timezone);
+
             $table
                 ->addRows([
                     [
                         $task->uuid,
+                        $createdDate->format($format),
                         $task->name,
                         $task->status,
                         $task->user->mail,
@@ -104,8 +122,8 @@ class InfoCommand extends AcquiaCommand
 
                 $timezone = new \DateTimeZone($tz);
 
-                $createdDate = new \DateTime($task->created_at);
-                $startedDate = new \DateTime($task->started_at);
+                $createdDate = new \DateTime($task->createdAt);
+                $startedDate = new \DateTime($task->startedAt);
                 $completedDate = new \DateTime($task->completed_at);
 
                 $createdDate->setTimezone($timezone);
