c557e024dac44e2eb17a6d43ea3ad84aa0f534d0
Adam Malone <adam@adammalone.net>
2018-01-23 09:31:30 +1100

    Fixes #12 for correct path to autoloader when included in other projects.

--- bin/acquiacli-robo.php
+++ bin/acquiacli-robo.php
@@ -14,9 +14,9 @@ if (strpos(basename(__FILE__), 'phar')) {
     if (file_exists(dirname(__DIR__).'/vendor/autoload.php')) {
         $root = dirname(__DIR__);
         require_once dirname(__DIR__) . '/vendor/autoload.php';
-    } elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
-        $root = __DIR__ . '/../../../';
-        require_once __DIR__ . '/../../../autoload.php';
+    } elseif (file_exists(dirname(__DIR__) . '/../../autoload.php')) {
+        $root = dirname(__DIR__) . '/../../../';
+        require_once dirname(__DIR__) . '/../../autoload.php';
     } else {
         $root = __DIR__;
         require_once 'phar://acquiacli.phar/vendor/autoload.php';
