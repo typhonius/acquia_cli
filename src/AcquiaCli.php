5ee633fbff051c9a1468bfc4c90d8a08b65cfa92
Adam Malone <adam@adammalone.net>
2018-04-24 23:29:27 +1000

    Adds semaphore lock to prevent multiple conflicting processes against Acquia API.

--- src/AcquiaCli.php
+++ src/AcquiaCli.php
@@ -5,6 +5,8 @@ namespace AcquiaCli;
 use Robo\Robo;
 use Robo\Config\Config;
 use Robo\Common\ConfigAwareTrait;
+use Symfony\Component\Lock\Factory;
+use Symfony\Component\Lock\Store\SemaphoreStore;
 use Robo\Runner as RoboRunner;
 use Robo\Application;
 use Symfony\Component\Console\Input\InputInterface;
@@ -76,7 +78,21 @@ class AcquiaCli
      */
     public function run(InputInterface $input, OutputInterface $output)
     {
+        // Obtain a lock and exit if the command is already running.
+        $store = new SemaphoreStore();
+        $factory = new Factory($store);
+        $lock = $factory->createLock('acquia-cli-command');
+
+        if (!$lock->acquire()) {
+            $output->writeln('The command is already running in another process.');
+
+            return 0;
+        }
+
         $statusCode = $this->runner->run($input, $output);
+
+        // Specifically release the lock after successful command invocation.
+        $lock->release();
 
         return $statusCode;
     }
