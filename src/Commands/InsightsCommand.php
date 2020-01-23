a94596cb02fcc85ecb692e96533ec2c2f643cd94
Adam Malone <adam@adammalone.net>
2020-01-24 09:21:20 +1100

    Adds alerts commands for insights.

--- src/Commands/InsightsCommand.php
+++ src/Commands/InsightsCommand.php
@@ -23,9 +23,8 @@ class InsightsCommand extends AcquiaCommand
      *
      * @command insights:info
      */
-    public function acquiaInsightsInfo($uuid, $environment = null)
+    public function insightsInfo($uuid, $environment = null)
     {
-
         $insightsAdapter = new Insights($this->cloudapi);
 
         if (null === $environment) {
@@ -38,6 +37,110 @@ class InsightsCommand extends AcquiaCommand
 
             $this->renderInsightInfo($insight);
         }
+    }
+
+    /**
+     * Shows insights alerts for specified applications.
+     *
+     * @param string $siteId
+     *
+     * @command insights:alerts:list
+     * @option failed Whether to only show failed insight checks.
+     */
+    public function insightsAlertsList($siteId, $options = ['failed' => null])
+    {
+        $insightsAdapter = new Insights($this->cloudapi);
+        $alerts = $insightsAdapter->getAllAlerts($siteId);
+
+        $output = $this->output();
+        $table = new Table($output);
+        $table->setHeaders(['UUID', 'Description', 'Failed', 'Resolved', 'Ignored']);
+        $table->setColumnStyle(2, 'center-align');
+        $table->setColumnStyle(3, 'center-align');
+        $table->setColumnStyle(4, 'center-align');
+
+        foreach ($alerts as $alert) {
+            /** @var InsightModuleResponse $module */
+
+            if ($options['failed'] && !$alert->failed_value) {
+                continue;
+            }
+
+            $table
+                ->addRows([
+                    [
+                        $alert->uuid,
+                        $alert->name,
+                        $alert->failed_value ? '✓' : '',
+                        $alert->flags->resolved ? '✓' : '',
+                        $alert->flags->ignored ? '✓' : '',
+                    ],
+                ]);
+        }
+
+        $table->render();
+    }
+
+    /**
+     * Shows insights alerts for specified applications.
+     *
+     * @param string $siteId
+     * @param string $alertUuid
+     *
+     * @command insights:alerts:get
+     */
+    public function insightsAlertsGet($siteId, $alertUuid)
+    {
+        $insightsAdapter = new Insights($this->cloudapi);
+        $alert = $insightsAdapter->getAlert($siteId, $alertUuid);
+
+        $this->say(sprintf('UUID: %s', $alert->uuid));
+        $this->say(sprintf('Name: %s', $alert->name));
+        $this->say(sprintf('Message: %s', filter_var($alert->message, FILTER_SANITIZE_STRING)));
+    }
+
+    /**
+     * Shows insights alerts for specified applications.
+     *
+     * @param string $siteId
+     * @option enabled Whether to only show enabled modules.
+     * @option upgradeable Whether to only show modules that need an upgrade.
+     *
+     * @command insights:modules
+     */
+    public function insightsModules($siteId, $options = ['enabled' => null, 'upgradeable' => null])
+    {
+        $insightsAdapter = new Insights($this->cloudapi);
+        $modules = $insightsAdapter->getModules($siteId);
+
+        $output = $this->output();
+        $table = new Table($output);
+        $table->setHeaders(['Name', 'Version', 'Enabled', 'Upgradeable']);
+        $table->setColumnStyle(2, 'center-align');
+        $table->setColumnStyle(3, 'center-align');
+
+        foreach ($modules as $module) {
+            /** @var InsightModuleResponse $module */
+
+            if ($options['enabled'] && !$module->flags->enabled) {
+                continue;
+            }
+            if ($options['upgradeable'] && array_search('upgradeable', $module->tags, true) === false) {
+                continue;
+            }
+
+            $table
+                ->addRows([
+                    [
+                        $module->name,
+                        $module->version,
+                        $module->flags->enabled ? '✓' : '',
+                        array_search('upgradeable', $module->tags, true) !== false ? '✓' : '',
+                    ],
+                ]);
+        }
+
+        $table->render();
     }
 
     /**
@@ -59,7 +162,8 @@ class InsightsCommand extends AcquiaCommand
         $format = "${char}  <fg=white;bg=${colour};options=bold>%s</fg=white;bg=${colour};options=bold>";
         $this->formattedOutput("${title} Score: " . $score, 20, $format);
 
-        $this->say('Status: ' . $insight->status);
+        $this->say(sprintf('Site ID: %s', $insight->uuid));
+        $this->say(sprintf('Status: %s', $insight->status));
 
         $output = $this->output();
         $table = new Table($output);
