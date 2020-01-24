28faeb301c6012b8f808f5d416b5d5b36af0d81f
Adam Malone <adam@adammalone.net>
2020-01-25 10:51:50 +1100

    Tweaks the drush alias automatic install process.

--- src/Commands/DrushAliasesCommand.php
+++ src/Commands/DrushAliasesCommand.php
@@ -4,7 +4,6 @@ namespace AcquiaCli\Commands;
 
 use AcquiaCloudApi\Response\EnvironmentResponse;
 use AcquiaCloudApi\Endpoints\Account;
-use PharData;
 
 /**
  * Class DrushAliasesCommand
@@ -15,7 +14,7 @@ class DrushAliasesCommand extends AcquiaCommand
 
     /**
      * Downloads a compressed archive of Drush aliases and prompts the user to unpack it in the right place.
-     * @option install Install Drush aliases directly.
+     * @option install Install Drush aliases directly without confirmation.
      *
      * @command drush:aliases
      */
@@ -24,33 +23,41 @@ class DrushAliasesCommand extends AcquiaCommand
         $account = new Account($this->cloudapi);
         $aliases = $account->getDrushAliases();
         $drushArchive = tempnam(sys_get_temp_dir(), 'AcquiaDrushAliases') . '.tar.gz';
+        $this->say(sprintf('Acquia Cloud Drush Aliases archive downloaded to %s', $drushArchive));
         if (file_put_contents($drushArchive, $aliases, LOCK_EX)) {
-            if ($options['install'] && $this->confirm(
+            if ($options['install'] || $this->confirm(
                 sprintf(
-                    'Are you sure you want to automatically unpack Acquia Cloud Drush Aliases to %s',
+                    'Do you want to automatically unpack Acquia Cloud Drush aliases to %s',
                     getenv('HOME')
                 )
             )) {
-                $drushDirectory = getenv('HOME') . '/.drush';
+                if (!$home = getenv('HOME')) {
+                    throw new \Exception('Home directory not found.');
+                }
+                $drushDirectory = $home . '/.drush';
                 if (!is_dir($drushDirectory)) {
                     mkdir($drushDirectory, 0700);
                 }
-                $archive = new PharData($drushArchive . '/.drush');
+                if (!is_writeable($drushDirectory)) {
+                    chmod($drushDirectory, 0700);
+                }
+                $archive = new \PharData($drushArchive . '/.drush');
                 $drushFiles = [];
                 foreach (new \RecursiveIteratorIterator($archive, \RecursiveIteratorIterator::LEAVES_ONLY) as $file) {
                     $drushFiles[] = '.drush/' . $file->getFileName();
                 }
-                $archive->extractTo(getenv('HOME'), $drushFiles, true);
+
+                $archive->extractTo($home, $drushFiles, true);
+                $this->say(sprintf('Acquia Cloud Drush aliases installed into %s', $drushDirectory));
+                unlink($drushArchive);
             } else {
-                $this->say(sprintf('Acquia Cloud Drush Aliases downloaded to %s', $drushArchive));
-                $this->say('Run the following command to install them:');
+                $this->say('Run the following command to install Drush aliases:');
                 $this->writeln(sprintf('$ tar -C $HOME -xf %s', $drushArchive));
                 $this->yell(
                     'This command will unpack into ~/.acquia and ~/.drush potentially overwriting existing files!',
                     40,
                     'yellow'
                 );
-                $this->say('Use the --install option to install aliases directly.');
             }
         } else {
             $this->say('Unable to download Drush Aliases');
