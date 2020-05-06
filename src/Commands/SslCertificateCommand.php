25cbfa1ad1548a3f5bc0e40a678bc16712695ca2
Thomas DENOLLE <tdenolle@pagesjaunes.fr>
2020-05-06 17:40:40 +0200

    Install, Enable and Disable SSLCertificateCommands

--- src/Commands/SslCertificateCommand.php
+++ src/Commands/SslCertificateCommand.php
@@ -86,4 +86,81 @@ class SslCertificateCommand extends AcquiaCommand
         $this->yell('Private Key');
         $this->writeln($certificate->private_key);
     }
+
+    /**
+     * Install an SSL certificate.
+     *
+     * @param string $uuid
+     * @param string $environment
+     * @param string $label
+     * @param string $cert
+     * @param string $key
+     * @param null|string $ca
+     *
+     * @command ssl:create
+     */
+    public function sslCertificateCreate(
+        OutputInterface $output,
+        SslCertificates $certificatesAdapter,
+        $uuid,
+        $environment,
+        $label,
+        $cert,
+        $key,
+        $ca = null
+    ) {
+        $environment = $this->cloudapiService->getEnvironment($uuid, $environment);
+        $this->say(sprintf('Installing new certificate (%s)', $label));
+        $certificatesAdapter->create(
+            $environment->uuid,
+            $label,
+            $cert,
+            $key,
+            $ca
+        );
+    }
+
+    /**
+     * Desactivates an active SSL certificate.
+     *
+     * @param string $uuid
+     * @param string $environment
+     * @param int    $certificateId
+     *
+     * @command ssl:disable
+     */
+    public function disable(
+        OutputInterface $output,
+        SslCertificates $certificatesAdapter,
+        $uuid,
+        $environment,
+        $certificateId
+    ) {
+        $environment = $this->cloudapiService->getEnvironment($uuid, $environment);
+        $certificate = $certificatesAdapter->get($environment->uuid, $certificateId);
+        $this->say(sprintf('Disabling certificate (%s)', $certificate->label));
+        $certificatesAdapter->disable($environment->uuid, $certificateId);
+    }
+
+    /**
+     * Activates an active SSL certificate.
+     *
+     * @param string $uuid
+     * @param string $environment
+     * @param int    $certificateId
+     *
+     * @command ssl:enable
+     */
+    public function enable(
+        OutputInterface $output,
+        SslCertificates $certificatesAdapter,
+        $uuid,
+        $environment,
+        $certificateId
+    ) {
+        $environment = $this->cloudapiService->getEnvironment($uuid, $environment);
+        $certificate = $certificatesAdapter->get($environment->uuid, $certificateId);
+        $this->say(sprintf('Enabling certificate (%s)', $certificate->label));
+        $certificatesAdapter->enable($environment->uuid, $certificateId);
+    }
 }
